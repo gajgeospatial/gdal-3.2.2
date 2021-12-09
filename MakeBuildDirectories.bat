f03032b8b734f611d5b3039c0e5cdbf81adc306e
gajgeospatial <glen.johnson@gaj-geospatial.com>
2021-12-08 21:13:37 -0500

    Removed old configurations; added vs2022

--- MakeBuildDirectories.bat
+++ MakeBuildDirectories.bat
@@ -1,117 +1,3 @@
-echo vs2010 Directories
-if not exist vc100 mkdir vc100
-if not exist vc100\\Win32 mkdir vc100\\Win32
-if not exist vc100\\x64 mkdir vc100\\x64
-if not exist vc100\\Win32\\vs2010_Debug mkdir vc100\\Win32\\vs2010_Debug
-if not exist vc100\\Win32\\vs2010_Debug\\gdalplugins mkdir vc100\\Win32\\vs2010_Debug\\gdalplugins
-if not exist vc100\\Win32\\vs2010_Release mkdir vc100\\Win32\\vs2010_Release
-if not exist vc100\\Win32\\vs2010_Release\\gdalplugins mkdir vc100\\Win32\\vs2010_Release\\gdalplugins
-if not exist vc100\\Win32\\bin mkdir vc100\\Win32\\bin
-if not exist vc100\\Win32\\bin\\gdalplugins mkdir vc100\\Win32\\bin\\gdalplugins
-if not exist vc100\\Win32\\bin\\java mkdir vc100\\Win32\\bin\\java
-if not exist vc100\\x64\\vs2010_Debug mkdir vc100\\x64\\vs2010_Debug
-if not exist vc100\\x64\\vs2010_Debug\\gdalplugins mkdir vc100\\x64\\vs2010_Debug\\gdalplugins
-if not exist vc100\\x64\\vs2010_Release mkdir vc100\\x64\\vs2010_Release
-if not exist vc100\\x64\\vs2010_Release\\gdalplugins mkdir vc100\\x64\\vs2010_Release\\gdalplugins
-if not exist vc100\\x64\\bin mkdir vc100\\x64\\bin
-if not exist vc100\\x64\\bin\\gdalplugins mkdir vc100\\x64\\bin\\gdalplugins
-if not exist vc100\\x64\\bin\\java mkdir vc100\\x64\\bin\\java
-if not exist vc100\\Win32\\data mkdir vc100\\Win32\\data
-if not exist vc100\\x64\\data mkdir vc100\\x64\\data
-copy /y data\* vc100\\Win32\\data
-copy /y data\* vc100\\x64\\data
-
-echo vs2012 Directories
-if not exist vc110 mkdir vc110
-if not exist vc110\\Win32 mkdir vc110\\Win32
-if not exist vc110\\x64 mkdir vc110\\x64
-if not exist vc110\\Win32\\vs2012_Debug mkdir vc110\\Win32\\vs2012_Debug
-if not exist vc110\\Win32\\vs2012_Debug\\gdalplugins mkdir vc110\\Win32\\vs2012_Debug\\gdalplugins
-if not exist vc110\\Win32\\vs2012_Release mkdir vc110\\Win32\\vs2012_Release
-if not exist vc110\\Win32\\vs2012_Release\\gdalplugins mkdir vc110\\Win32\\vs2012_Release\\gdalplugins
-if not exist vc110\\Win32\\bin mkdir vc110\\Win32\\bin
-if not exist vc110\\Win32\\bin\\gdalplugins mkdir vc110\\Win32\\bin\\gdalplugins
-if not exist vc110\\Win32\\bin\\java mkdir vc110\\Win32\\bin\\java
-if not exist vc110\\x64\\vs2012_Debug mkdir vc110\\x64\\vs2012_Debug
-if not exist vc110\\x64\\vs2012_Debug\\gdalplugins mkdir vc110\\x64\\vs2012_Debug\\gdalplugins
-if not exist vc110\\x64\\vs2012_Release mkdir vc110\\x64\\vs2012_Release
-if not exist vc110\\x64\\vs2012_Release\\gdalplugins mkdir vc110\\x64\\vs2012_Release\\gdalplugins
-if not exist vc110\\x64\\bin mkdir vc110\\x64\\bin
-if not exist vc110\\x64\\bin\\gdalplugins mkdir vc110\\x64\\bin\\gdalplugins
-if not exist vc110\\x64\\bin\\java mkdir vc110\\x64\\bin\\java
-if not exist vc110\\Win32\\data mkdir vc110\\Win32\\data
-if not exist vc110\\x64\\data mkdir vc110\\x64\\data
-copy /y data\* vc110\\Win32\\data
-copy /y data\* vc110\\x64\\data
-
-echo vs2013 Directories
-if not exist vc120 mkdir vc120
-if not exist vc120\\Win32 mkdir vc120\\Win32
-if not exist vc120\\x64 mkdir vc120\\x64
-if not exist vc120\\Win32\\Debug mkdir vc120\\Win32\\Debug
-if not exist vc120\\Win32\\Debug\\gdalplugins mkdir vc120\\Win32\\Debug\\gdalplugins
-if not exist vc120\\Win32\\Release mkdir vc120\\Win32\\Release
-if not exist vc120\\Win32\\Release\\gdalplugins mkdir vc120\\Win32\\Release\\gdalplugins
-if not exist vc120\\Win32\\bin mkdir vc120\\Win32\\bin
-if not exist vc120\\Win32\\bin\\gdalplugins mkdir vc120\\Win32\\bin\\gdalplugins
-if not exist vc120\\Win32\\bin\\java mkdir vc120\\Win32\\bin\\java
-if not exist vc120\\x64\\Debug mkdir vc120\\x64\\Debug
-if not exist vc120\\x64\\Debug\\gdalplugins mkdir vc120\\x64\\Debug\\gdalplugins
-if not exist vc120\\x64\\Release mkdir vc120\\x64\\Release
-if not exist vc120\\x64\\Release\\gdalplugins mkdir vc120\\x64\\Release\\gdalplugins
-if not exist vc120\\x64\\bin mkdir vc120\\x64\\bin
-if not exist vc120\\x64\\bin\\gdalplugins mkdir vc120\\x64\\bin\\gdalplugins
-if not exist vc120\\x64\\bin\\java mkdir vc120\\x64\\bin\\java
-if not exist vc120\\Win32\\data mkdir vc120\\Win32\\data
-if not exist vc120\\x64\\data mkdir vc120\\x64\\data
-copy /y data\* vc120\\Win32\\data
-copy /y data\* vc120\\x64\\data
-
-echo vs2015 Directories
-if not exist vc140 mkdir vc140
-if not exist vc140\\Win32 mkdir vc140\\Win32
-if not exist vc140\\x64 mkdir vc140\\x64
-if not exist vc140\\Win32\\vs2015_Debug mkdir vc140\\Win32\\vs2015_Debug
-if not exist vc140\\Win32\\vs2015_Debug\\gdalplugins mkdir vc140\\Win32\\vs2015_Debug\\gdalplugins
-if not exist vc140\\Win32\\vs2015_Release mkdir vc140\\Win32\\vs2015_Release
-if not exist vc140\\Win32\\vs2015_Release\\gdalplugins mkdir vc140\\Win32\\vs2015_Release\\gdalplugins
-if not exist vc140\\Win32\\bin mkdir vc140\\Win32\\bin
-if not exist vc140\\Win32\\bin\\gdalplugins mkdir vc140\\Win32\\bin\\gdalplugins
-if not exist vc140\\Win32\\bin\\java mkdir vc140\\Win32\\bin\\java
-if not exist vc140\\x64\\vs2015_Debug mkdir vc140\\x64\\vs2015_Debug
-if not exist vc140\\x64\\vs2015_Debug\\gdalplugins mkdir vc140\\x64\\vs2015_Debug\\gdalplugins
-if not exist vc140\\x64\\vs2015_Release mkdir vc140\\x64\\vs2015_Release
-if not exist vc140\\x64\\vs2015_Release\\gdalplugins mkdir vc140\\x64\\vs2015_Release\\gdalplugins
-if not exist vc140\\x64\\bin mkdir vc140\\x64\\bin
-if not exist vc140\\x64\\bin\\gdalplugins mkdir vc140\\x64\\bin\\gdalplugins
-if not exist vc140\\x64\\bin\\java mkdir vc140\\x64\\bin\\java
-if not exist vc140\\Win32\\data mkdir vc140\\Win32\\data
-if not exist vc140\\x64\\data mkdir vc140\\x64\\data
-copy /y data\* vc140\\Win32\\data
-copy /y data\* vc140\\x64\\data
-
-echo vs2017 Directories
-if not exist vc141 mkdir vc141
-if not exist vc141\\Win32 mkdir vc141\\Win32
-if not exist vc141\\x64 mkdir vc141\\x64
-if not exist vc141\\Win32\\vs2017_Debug mkdir vc141\\Win32\\vs2017_Debug
-if not exist vc141\\Win32\\vs2017_Debug\\gdalplugins mkdir vc141\\Win32\\vs2017_Debug\\gdalplugins
-if not exist vc141\\Win32\\vs2017_Release mkdir vc141\\Win32\\vs2017_Release
-if not exist vc141\\Win32\\vs2017_Release\\gdalplugins mkdir vc141\\Win32\\vs2017_Release\\gdalplugins
-if not exist vc141\\Win32\\bin mkdir vc141\\Win32\\bin
-if not exist vc141\\Win32\\bin\\gdalplugins mkdir vc141\\Win32\\bin\\gdalplugins
-if not exist vc141\\Win32\\bin\\java mkdir vc141\\Win32\\bin\\java
-if not exist vc141\\x64\\vs2017_Debug mkdir vc141\\x64\\vs2017_Debug
-if not exist vc141\\x64\\vs2017_Debug\\gdalplugins mkdir vc141\\x64\\vs2017_Debug\\gdalplugins
-if not exist vc141\\x64\\vs2017_Release mkdir vc141\\x64\\vs2017_Release
-if not exist vc141\\x64\\vs2017_Release\\gdalplugins mkdir vc141\\x64\\vs2017_Release\\gdalplugins
-if not exist vc141\\x64\\bin mkdir vc141\\x64\\bin
-if not exist vc141\\x64\\bin\\gdalplugins mkdir vc141\\x64\\bin\\gdalplugins
-if not exist vc141\\x64\\bin\\java mkdir vc141\\x64\\bin\\java
-if not exist vc141\\Win32\\data mkdir vc141\\Win32\\data
-if not exist vc141\\x64\\data mkdir vc141\\x64\\data
-copy /y data\* vc141\\Win32\\data
-copy /y data\* vc141\\x64\\data
 
 echo vs2019 Directories
 if not exist vc142 mkdir vc142
@@ -135,4 +21,27 @@ if not exist vc142\\Win32\\data mkdir vc142\\Win32\\data
 if not exist vc142\\x64\\data mkdir vc142\\x64\\data
 copy /y data\* vc142\\Win32\\data
 copy /y data\* vc142\\x64\\data
+
+echo vs2022 Directories
+if not exist vc143 mkdir vc143
+if not exist vc143\\Win32 mkdir vc143\\Win32
+if not exist vc143\\x64 mkdir vc143\\x64
+if not exist vc143\\Win32\\vs2022_Debug mkdir vc143\\Win32\\vs2022_Debug
+if not exist vc143\\Win32\\vs2022_Debug\\gdalplugins mkdir vc143\\Win32\\vs2022_Debug\\gdalplugins
+if not exist vc143\\Win32\\vs2022_Release mkdir vc143\\Win32\\vs2022_Release
+if not exist vc143\\Win32\\vs2022_Release\\gdalplugins mkdir vc143\\Win32\\vs2022_Release\\gdalplugins
+if not exist vc143\\Win32\\bin mkdir vc143\\Win32\\bin
+if not exist vc143\\Win32\\bin\\gdalplugins mkdir vc143\\Win32\\bin\\gdalplugins
+if not exist vc143\\Win32\\bin\\java mkdir vc143\\Win32\\bin\\java
+if not exist vc143\\x64\\vs2022_Debug mkdir vc143\\x64\\vs2022_Debug
+if not exist vc143\\x64\\vs2022_Debug\\gdalplugins mkdir vc143\\x64\\vs2022_Debug\\gdalplugins
+if not exist vc143\\x64\\vs2022_Release mkdir vc143\\x64\\vs2022_Release
+if not exist vc143\\x64\\vs2022_Release\\gdalplugins mkdir vc143\\x64\\vs2022_Release\\gdalplugins
+if not exist vc143\\x64\\bin mkdir vc143\\x64\\bin
+if not exist vc143\\x64\\bin\\gdalplugins mkdir vc143\\x64\\bin\\gdalplugins
+if not exist vc143\\x64\\bin\\java mkdir vc143\\x64\\bin\\java
+if not exist vc143\\Win32\\data mkdir vc143\\Win32\\data
+if not exist vc143\\x64\\data mkdir vc143\\x64\\data
+copy /y data\* vc143\\Win32\\data
+copy /y data\* vc143\\x64\\data
 
